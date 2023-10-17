db65244b2202b32899bbb5a1890a961b2cd93dd0
Aniel Flores-Lopez <aniel.flores4@gmail.com>
2023-10-16 20:14:15 -0700

    webserver

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+Author = Aniel Flores-Lopez
+Email = aflores9@uoregon.edu
+Description = webserver made using docker and flask
+
+
 # UOCIS322 - Project 2 #
 
 This project will get you started with Docker and Flask. You need to have Docker set up on your machine to complete this project. You can alternatively use the machine we talked about in class.
--- web/app.py
+++ web/app.py
@@ -1,8 +1,9 @@
 """
 John Doe's Flask API.
 """
-
-from flask import Flask
+import os
+import configparser
+from flask import Flask,abort,send_from_directory,render_template
 
 app = Flask(__name__)
 
@@ -10,5 +11,40 @@ app = Flask(__name__)
 def hello():
     return "UOCIS docker demo!\n"
 
+
+@app.route("/<string:file>")
+def files(file):
+    try:
+        if "~" in file or ".." in file:
+            return send_from_directory("pages/","403.html"),403
+        return send_from_directory("pages/",file),200
+    except:
+        return send_from_directory("pages/","404.html"),404
+    
+
+
+def parse_config(config_paths):
+    config_path = None
+    for f in config_paths:
+        if os.path.isfile(f):
+            config_path = f
+            break
+
+    if config_path is None:
+        raise RuntimeError("Configuration file not found!")
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    return config
+
+config = parse_config(["credentials.ini", "default.ini"])
+port = config["SERVER"]["PORT"]
+debug = config["SERVER"]["DEBUG"]
+
+
+
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=debug, host='0.0.0.0',port=port)
